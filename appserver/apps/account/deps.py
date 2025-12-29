f204b1760c7748608f10dc64836b59093b259aa2
tmk5415 <tmk5415@withrobot.com>
2025-12-29 17:10:07 +0900

    캘린더 관련 예외 추가

--- appserver/apps/account/deps.py
+++ appserver/apps/account/deps.py
@@ -7,13 +7,11 @@ from .exceptions import InvalidTokenError, ExpiredTokenError
 from .models import User
 from .utils import decode_token, ACCESS_TOKEN_EXPIRE_MINUTES
 from .exceptions import InvalidTokenError, ExpiredTokenError, UserNotFoundError
+from sqlalchemy.ext.asyncio import AsyncSession
 
-async def get_current_user(
-    auth_token: Annotated[str, Cookie(...)],
-    db_session: DbSessionDep,
-):
-    if auth_token is None:
-        raise InvalidTokenError()
+async def get_user(auth_token: str | None, db_session: AsyncSession) -> User | None:
+    if not auth_token:
+        return None
     
     try:
         decoded = decode_token(auth_token)
@@ -27,10 +25,43 @@ async def get_current_user(
     
     stmt = select(User).where(User.username == decoded["sub"])
     result = await db_session.execute(stmt)
-    user = result.scalar_one_or_none()
+    
+    return result.scalar_one_or_none()
+
+async def get_current_user(
+    auth_token: Annotated[str, Cookie(...)],
+    db_session: DbSessionDep,
+):
+    user = await get_user(auth_token, db_session)
+    
+    # if auth_token is None:
+    #     raise InvalidTokenError()
+    
+    # try:
+    #     decoded = decode_token(auth_token)
+    # except Exception as e:
+    #     raise InvalidTokenError() from e
+    
+    # expires_at = datetime.fromtimestamp(decoded["exp"], tz=timezone.utc)
+    # now = datetime.now(timezone.utc)
+    # if now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) < expires_at:
+    #     raise ExpiredTokenError()
+    
+    # stmt = select(User).where(User.username == decoded["sub"])
+    # result = await db_session.execute(stmt)
+    # user = result.scalar_one_or_none()
     
     if user in None:
         raise UserNotFoundError()
     return user
 
 CurrentUserDep = Annotated[User, Depends(get_current_user)]
+
+async def get_current_user_optional(
+    db_session: DbSessionDep,
+    auth_token: Annotated[str | None, Cookie()] = None,
+):
+    user = await get_user(auth_token, db_session)
+    return user
+
+CurrentUserOptionalDep = Annotated[User | None, Depends(get_current_user_optional)]
---
+++ appserver/apps/calendar/endpoints.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, status
+from sqlmodel import select
+from appserver.apps.account.models import User
+from appserver.apps.calendar.models import Calendar
+from appserver.db import DbSessionDep
+from appserver.apps.account.deps import CurrentUserOptionalDep
+from .schemas import CalendarDetailOut, CalendarOut
+from .exception import CalendarNotFoundError, HostNotFoundError
+
+router = APIRouter()
+
+async def host_calendar_detail(
+    host_username: str,
+    user: CurrentUserOptionalDep,
+    session: DbSessionDep
+) -> CalendarOut | CalendarDetailOut:
+    stmt = select(User).where(User.username == host_username)
+    result = await session.execute(stmt)
+    host = result.scalar_one_or_none
+    if host is None:
+        raise HostNotFoundError()
+    
+    stmt = select((Calendar).where(Calendar.host_id) == host.id)
+    result = await session.execute(stmt)
+    calendar = result.scalar_one_or_none()
+    if user is not None and user.id == host.id:
+        return CalendarDetailOut.model_validate(calendar)
+    
+    return CalendarOut.model_validate(calendar)
